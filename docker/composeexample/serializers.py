b18a673eb5aef5e4946b96a9388e90dbf5661c8b
DLis117 <83869800+DLis117@users.noreply.github.com>
2021-11-16 20:48:05 +0100

    Delete docker directory

--- docker/composeexample/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import Idea
-class IdeaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Idea
-        fields=['id','title','description']
